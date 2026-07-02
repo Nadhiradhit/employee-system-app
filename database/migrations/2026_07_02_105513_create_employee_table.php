e2ee37fb86189637feb60b5f656a5edc46181579
Nadhiradhit <nadhiradhitya@gmail.com>
2026-07-02 21:17:25 +0700

    feat: first add dependencies, migration database, seeder

---
+++ database/migrations/2026_07_02_105513_create_employee_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('user_id')->nullable();
+            $table->integer('phone_number')->nullable();
+            $table->string('department')->nullable();
+            $table->date('joining_date')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee');
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'id' => \Illuminate\Support\Str::uuid(),
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'is_admin' => true,
+        ]);
+
+        User::factory()->count(20)->create();
+    }
+}
